9e9a6cefab5ec62267fc539ea5d7e91fe040913c
merlinmb <markbeets@gmail.com>
2026-04-16 07:55:07 +0100

    feat: add timezone field to AppConfig with NVS persistence

--- include/config.h
+++ include/config.h
@@ -6,6 +6,7 @@ struct AppConfig {
     char wifi_password[64];
     char server_url[256];
     char read_token[128];
+    char timezone[48];
     uint16_t refresh_secs;
 };
 
--- src/config.cpp
+++ src/config.cpp
@@ -13,6 +13,8 @@ void config_defaults(AppConfig &cfg) {
     cfg.wifi_password[0] = '\0';
     cfg.read_token[0]    = '\0';
     cfg.refresh_secs     = 300;
+    strncpy(cfg.timezone, "Europe/London", sizeof(cfg.timezone) - 1);
+    cfg.timezone[sizeof(cfg.timezone) - 1] = '\0';
 }
 
 void config_load(AppConfig &cfg) {
@@ -24,6 +26,7 @@ void config_load(AppConfig &cfg) {
     prefs.getString("wifi_pass",  cfg.wifi_password, sizeof(cfg.wifi_password));
     prefs.getString("server_url", cfg.server_url,    sizeof(cfg.server_url));
     prefs.getString("read_token", cfg.read_token,    sizeof(cfg.read_token));
+    prefs.getString("timezone", cfg.timezone, sizeof(cfg.timezone));
     cfg.refresh_secs = prefs.getUShort("refresh_secs", 300);
     if (cfg.refresh_secs < 60 || cfg.refresh_secs > 3600) cfg.refresh_secs = 300;
     prefs.end();
@@ -36,6 +39,7 @@ void config_save(const AppConfig &cfg) {
     prefs.putString("wifi_pass",    cfg.wifi_password);
     prefs.putString("server_url",   cfg.server_url);
     prefs.putString("read_token",   cfg.read_token);
+    prefs.putString("timezone",    cfg.timezone);
     prefs.putUShort("refresh_secs", cfg.refresh_secs);
     prefs.end();
 }
